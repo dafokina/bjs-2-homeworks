b075e0b00b21591e5449013779e4e92f928a2fff
Дарья Фокина <cool.russia-33@yandex.ru>
2023-02-04 20:17:19 +0300

    added solution of the tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(user => user.gender === gender).map(result => result.age).reduce((summ, userAge, index, array) => {
+        summ += userAge;
+        if (index === array.length - 1) {
+          return summ / array.length;
+        }
+        return summ;
+      }, 0);
 }
